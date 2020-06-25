875c57bc97f993f8bc8652617e931a2ce92a4272
ercileycoimbra <60478976+ercileycoimbra@users.noreply.github.com>
2020-06-25 15:18:58 -0300

    Create html.go

---
+++ html.go
@@ -0,0 +1,25 @@
+package html
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// Titulo obtem o título de uma janela html
+func Titulo(urls ...string) <-chan string {
+	c := make(chan string) 
+
+	for _, url := range urls {
+		go func(cUrl string) {
+			resp, _ := http.Get(cUrl)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+
+	return c
+}
